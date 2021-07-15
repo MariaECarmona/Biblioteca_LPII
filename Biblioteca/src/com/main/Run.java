48ace024e59934de9f11ae9b2d14f95f2dbbb21d
MariaECarmona <dudacarmona1802@gmil.com>
2021-07-14 22:11:38 -0300

    Início da implementação da interface gráfica

---
+++ Biblioteca/src/com/main/Run.java
@@ -0,0 +1,9 @@
+ package com.main;
+import javafx.application.Application;
+
+ public class Run extends Application {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Biblioteca/src/com/view/View.java
@@ -0,0 +1,15 @@
+package com.view;
+
+import javax.swing.*;
+
+public class View extends JFrame{
+    private JTabbedPane painelAbas;
+    private JPanel painelLivros;
+
+    public View(){
+        painelAbas = new JTabbedPane();
+        painelLivros = new JPanel();
+
+        painelAbas
+    }
+}
